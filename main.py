ca67e1b896a9fbbd60175e3e385b71e30de7f8ec
antonio murilo <107326926+MuriloAraujo174@users.noreply.github.com>
2025-01-14 11:25:30 -0300

    new update

---
+++ main.py
@@ -0,0 +1,17 @@
+# print("Hello World")
+
+import pyautogui
+import time
+
+pyautogui.PAUSE = .5
+
+# pyautogui.click
+# pyautogui.press
+# pyautogui.scroll
+# pyautogui.write
+
+pyautogui.press("win")
+pyautogui.write("edger")
+pyautogui.press("enter")
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
